6ce82188622e25eb6a3708ccf68c7e33ef20be30
yellareddy-m <35562898+yellareddy-m@users.noreply.github.com>
2022-08-26 10:12:32 +0530

    Time: 169 ms (41.07%), Space: 48.4 MB (78.84%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -2,20 +2,41 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// function sortedSquares(nums) {
+//     let start = 0;
+//     let end = nums.length - 1;
+//     const sortedArray = new Array(nums.length).fill(0);
+//     for (let idx = nums.length - 1; idx >= 0; idx--) {
+//         const smallValue = nums[start];
+//         const largeValue = nums[end];
+//         if (Math.abs(smallValue) > Math.abs(largeValue)) {
+//             sortedArray[idx] = smallValue * smallValue;
+//             start++;
+//             } else {
+//                 sortedArray[idx] = largeValue * largeValue;
+//                 end--;
+//             }
+//     }
+//     return sortedArray;
+// }
+
 function sortedSquares(nums) {
-    let start = 0;
-    let end = nums.length - 1;
-    const sortedArray = new Array(nums.length).fill(0);
-    for (let idx = nums.length - 1; idx >= 0; idx--) {
-        const smallValue = nums[start];
-        const largeValue = nums[end];
-        if (Math.abs(smallValue) > Math.abs(largeValue)) {
-            sortedArray[idx] = smallValue * smallValue;
-            start++;
-            } else {
-                sortedArray[idx] = largeValue * largeValue;
-                end--;
-            }
+  let squares = [],
+      left = 0,
+      right = nums.length - 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+      const rightNum = nums[right];
+      const leftNum = nums[left];
+      if (Math.abs(leftNum) > Math.abs(rightNum)) {
+        squares[i] = leftNum * leftNum;
+        left++;
+      } else {
+        squares[i] = rightNum * rightNum;
+        right--;
+      }
+
     }
-    return sortedArray;
-}
+  
+  // TODO: Write your code here
+  return squares;
+};
