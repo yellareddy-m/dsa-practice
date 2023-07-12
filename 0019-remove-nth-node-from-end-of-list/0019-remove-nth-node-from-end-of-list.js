e1fe35f011caed2e345c8d313001b636fcd63fce
yellareddy-m <m.yellareddy6@gmail.com>
2023-07-12 20:27:23 +0530

    Time: 70 ms (17.78%), Space: 42.1 MB (97.28%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+
+function getLength(node) {
+    let length = 1;
+    while (node.next) {
+        length++;
+        node = node.next;
+    }
+    return length;
+}
+var removeNthFromEnd = function (head, n) {
+
+    // Naive solution
+    let temp = head;
+    let length = getLength(head);
+    if(n === length) {
+        return head.next;
+    }
+    const prevNodeIndex = length - n;
+   let curr = null;
+    // 1 2 3 4 5 6          2
+    for (let i = 0; i < prevNodeIndex; i++) {
+        prev = temp;
+        temp = temp.next;
+    }
+    prev.next = temp.next;
+
+    return head;
+
+};
