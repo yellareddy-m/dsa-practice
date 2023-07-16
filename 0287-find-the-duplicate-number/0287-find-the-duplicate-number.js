9e3fe7cf8e3c0ed80c711a007fc8f0221e90df54
yellareddy-m <m.yellareddy6@gmail.com>
2023-07-16 10:14:30 +0530

    Time: 98 ms (49.95%), Space: 50 MB (64.67%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -4,11 +4,29 @@
  */
 var findDuplicate = function(nums) {
     // using hashMap
-    let set = new Set();
-    for (let i = 0; i < nums.length; i++) {
-        if (set.has(nums[i])) {
-            return nums[i];
+    // let set = new Set();
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (set.has(nums[i])) {
+    //         return nums[i];
+    //     }
+    //     set.add(nums[i]);
+    // }
+    
+    // using Floyd's Cycle Detection algorithm
+    
+    let slow = nums[0];
+    let fast = nums[0];
+    while(true) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (fast === slow) {
+            break;    
         }
-        set.add(nums[i]);
     }
+    let secondSlow = nums[0];
+    while (secondSlow !== slow) {
+        slow = nums[slow];
+        secondSlow = nums[secondSlow];
+    }
+    return slow;
 };
