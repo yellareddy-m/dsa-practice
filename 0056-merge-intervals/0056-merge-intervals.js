5a8c161658ded4f986d24b7fa4d557fd7dc28aa0
yellareddy-m <m.yellareddy6@gmail.com>
2023-07-21 15:43:30 +0530

    Time: 121 ms (22.09%), Space: 51.1 MB (23.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,25 +3,12 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    // intervals.sort((a, b) => a - b )
-    // for (let i = 0; i < intervals.length - 1; i++) {
-    //     const [start1, end1] = intervals[i];
-    //     const [start2, end2] = intervals[i + 1];
-    //     if (end1 >= start2) {
-    //         intervals.splice(i, 2, [Math.min(start1, start2), Math.max(end1, end2)]);
-    //         i--;
-    //     }
-    // }
-    // return intervals;
-    intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]);
-    
-    for(let i = 0; i < intervals.length-1; i++) {
+    intervals.sort((a, b) => a[0] - b[0] )
+    for (let i = 0; i < intervals.length - 1; i++) {
         const [start1, end1] = intervals[i];
-        const [start2, end2] = intervals[i+1];
-        
-        if(start2 <= end1) {
-            const newInterval = [Math.min(start1, start2), Math.max(end1, end2)];
-            intervals.splice(i, 2, newInterval);
+        const [start2, end2] = intervals[i + 1];
+        if (end1 >= start2) {
+            intervals.splice(i, 2, [Math.min(start1, start2), Math.max(end1, end2)]);
             i--;
         }
     }
