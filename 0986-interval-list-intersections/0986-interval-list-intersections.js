72196f1dd121268cabe36d24b24ace8b04ac7de7
yellareddy-m <m.yellareddy6@gmail.com>
2023-07-25 21:54:18 +0530

    Time: 101 ms (34.52%), Space: 50.3 MB (16.67%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.js
+++ 0986-interval-list-intersections/0986-interval-list-intersections.js
@@ -5,19 +5,34 @@
  */
 var intervalIntersection = function(firstList, secondList) {
     let result = [];
-    for (let i = 0; i < firstList.length; i++) {
+    // for (let i = 0; i < firstList.length; i++) {
+    //     const [start1, end1] = firstList[i];
+    //     for (let j = 0; j < secondList.length; j++) {
+    //         const [start2, end2] = secondList[j];
+    //         // maximum of starts [0,2] and  [1,5]  = ([1,2]) is the overlapping start.
+    //         const overlappingStart = Math.max(start1, start2);
+    //         // minimum of starts [0,2] and  [1,5]   =  ([1,2]) is the overlapping end.
+    //         const overlappingEnd = Math.min(end1,end2);
+    //         // not a valid interval
+    //         if (overlappingStart > overlappingEnd) {
+    //             continue;
+    //         }
+    //         result.push([overlappingStart, overlappingEnd]);
+    //     }
+    // }
+    let i = 0, j= 0;
+    while (i < firstList.length && j < secondList.length) {
         const [start1, end1] = firstList[i];
-        for (let j = 0; j < secondList.length; j++) {
-            const [start2, end2] = secondList[j];
-            // maximum of starts [0,2] and  [1,5]  = ([1,2]) is the overlapping start.
-            const overlappingStart = Math.max(start1, start2);
-            // minimum of starts [0,2] and  [1,5]   =  ([1,2]) is the overlapping end.
-            const overlappingEnd = Math.min(end1,end2);
-            // not a valid interval
-            if (overlappingStart > overlappingEnd) {
-                continue;
-            }
-            result.push([overlappingStart, overlappingEnd]);
+        const [start2, end2] = secondList[j];
+        let overlappingStart = Math.max(start1, start2);
+        let overlappingEnd = Math.min(end1, end2);
+        if (overlappingStart <= overlappingEnd) {
+            result.push([overlappingStart, overlappingEnd])
+        }
+        if (end1 < end2) {
+            i++;
+        } else{
+            j++;
         }
     }
     return result;
