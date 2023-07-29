efecd6daa6280a01c29301159b8393b1753dce28
yellareddy-m <m.yellareddy6@gmail.com>
2023-07-29 20:04:57 +0530

    Time: 76 ms (56.49%), Space: 41.7 MB (95.93%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // NAIVE
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    
+    // SORTING SOLUTION
+    // nums.sort((a, b) => a -b);
+    // let left = 0;
+    // let right = nums.length - 1;
+    // let sum = 0;
+    // while(left < right) {
+    //     sum = nums[left] + nums[right];
+    //     if (sum < target) {
+    //         left++;
+    //     } else if (sum > target) {
+    //         right--;
+    //     } else {
+    //         return [left, right];
+    //     }
+    // }
+};
