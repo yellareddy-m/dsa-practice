387d5cf8889e644878c98a383262bd1b9c3a32dc
yellareddy-m <m.yellareddy6@gmail.com>
2023-07-31 20:06:21 +0530

    Time: 3322 ms (9.81%), Space: 55.5 MB (33.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,14 +4,27 @@
  */
 var productExceptSelf = function(nums) {
     let result = [];
+    // for(let i = 0; i < nums.length; i++) {
+    //     let product = 1;
+    //     for (let j = 0; j < nums.length; j++) {
+    //         if (i !== j) {
+    //             product *= nums[j];
+    //         }
+    //     }
+    //     result.push(product);
+    // }
+    
+    // ANOTHER WAY COMPUTING LEFT AND RIGHT IN NESTED LOOPS
+    let left = 1;
     for(let i = 0; i < nums.length; i++) {
         let product = 1;
-        for (let j = 0; j < nums.length; j++) {
-            if (i !== j) {
-                product *= nums[j];
-            }
+        
+        for (let j = i + 1; j < nums.length; j++) {
+            product *= nums[j];
         }
-        result.push(product);
+        result.push(product * left);
+        left = left * nums[i];
     }
+    
     return result;
 };
