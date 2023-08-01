c0b2cde3fbc357d46e8b322c9ea70281dc1cd670
yellareddy-m <m.yellareddy6@gmail.com>
2023-08-01 19:43:33 +0530

    Time: 60 ms (85.20%), Space: 44 MB (72.96%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.js
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.js
@@ -9,14 +9,26 @@ var secondHighest = function(s) {
             numbersArray.push(Number(s[i]));    
         }
     }
-    let largest = -Infinity;
-    for (let i = 0; i < numbersArray.length; i++) {
-        largest = Math.max(largest, numbersArray[i])
-    }
+    let largest = -1;
+    // for (let i = 0; i < numbersArray.length; i++) {
+    //     largest = Math.max(largest, numbersArray[i])
+    // }
     let secondLargest = -1;
-    for (let j = 0; j < numbersArray.length; j++) {
-        if (numbersArray[j] !== largest) {
-            secondLargest = Math.max(secondLargest, numbersArray[j]);
+    // for (let j = 0; j < numbersArray.length; j++) {
+    //     if (numbersArray[j] !== largest) {
+    //         secondLargest = Math.max(secondLargest, numbersArray[j]);
+    //     }
+    // }
+    
+    // USING ONLY ONE FOR LOOP
+    
+    for (let i = 0; i < numbersArray.length; i++) {
+        const currentNum = numbersArray[i];
+        if (currentNum > largest) {
+            secondLargest = largest;
+            largest = currentNum;
+        } else if (currentNum > secondLargest && currentNum < largest) {
+               secondLargest = currentNum;
         }
     }
     return secondLargest;
