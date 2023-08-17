065d4532160f823a27dcec5574a203b97324dff2
yellareddy-m <m.yellareddy6@gmail.com>
2023-08-17 16:55:08 +0530

    Time: 52 ms (70.96%), Space: 41.5 MB (88.04%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
@@ -0,0 +1,65 @@
+var MyQueue = function() {
+    this.arr = [];
+    this.push = function(value) {
+        this.arr.push(value);
+    }
+    this.pop = function() {
+        return this.arr.shift();
+    }
+    this.empty = function() {
+        return this.arr.length === 0;
+    }
+    this.peek = function() {
+        return this.arr[0];
+    }
+    this.size = function() {
+        return this.arr.length;
+    }
+}
+
+var MyStack = function() {
+    this.queue = new MyQueue();
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.push(x);
+    let size = this.queue.size();
+    while(size > 1) {
+        this.queue.push(this.queue.pop());
+        size--;
+    }
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.queue.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue.peek();
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue.empty();
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
