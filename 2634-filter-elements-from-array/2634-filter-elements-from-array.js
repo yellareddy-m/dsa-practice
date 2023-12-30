b709adb7dce731c968f08e248b3ba40a0e6ea085
yellareddy-m <m.yellareddy6@gmail.com>
2023-12-30 15:25:16 +0530

    Time: 108 ms (5.78%), Space: 41.6 MB (85.22%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -5,7 +5,7 @@
  */
 var filter = function(arr, fn) {
     const filteredArray = [];
-    for (let i = 0; i < arr.length; i++) {
+    for(let i = 0; i < arr.length; i++) {
         if (fn(arr[i], i)) {
             filteredArray.push(arr[i]);
         }
