285592f11756d35eedc7f35050367fcb9927f20b
yellareddy-m <m.yellareddy6@gmail.com>
2023-12-30 21:39:39 +0530

    Time: 90 ms (5.61%), Space: 43.4 MB (46.15%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,11 +3,13 @@
  * @return {Function}
  */
 var compose = function(functions) {
-	return function(x) {
-        functions.reverse().forEach(fn => {
-            x = fn(x);
-        });
-        return x;
+    
+    return function(x) {
+        let result = x;
+        for (let i = functions.length - 1; i >= 0; i--) {
+           result = functions[i](result)
+        }
+        return result;
     }
 };
 
