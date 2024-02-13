a52b32f5f0d770dd63e778ede755fafae0e8589b
yellareddy-m <m.yellareddy6@gmail.com>
2024-02-13 18:55:57 +0530

    Time: 45 ms (91.77%), Space: 49.1 MB (6.22%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -3,7 +3,7 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    return new Promise((resolve, reject) => setTimeout(() => resolve(), millis))
+    return new Promise((resolve, reject) => { setTimeout(()=>resolve(), millis)});
 }
 
 /** 
