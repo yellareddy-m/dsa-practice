171bd41eb532fdc488b5c69b9c38678396bfb473
yellareddy-m <m.yellareddy6@gmail.com>
2024-02-18 18:04:19 +0530

    Time: 82 ms (57.95%), Space: 57.6 MB (26.98%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -17,6 +17,17 @@ var rotate = function(nums, k) {
     
     // (nums.splice(nums.length - k)).concat(nums.splice(0, nums.length))
     k = k % nums.length;
-    const details = nums.splice(nums.length - k);
-    nums.unshift(...details);
+    // const details = nums.splice(nums.length - k);
+    // nums.unshift(...details);
+    
+    function reverse(i, j){
+        while (i < j){
+            [nums[j], nums[i]] = [nums[i], nums[j]];
+            i++;
+            j--;
+        }
+    }
+    reverse(0, nums.length-1); // 7,6,5,4,3,2,1
+    reverse(0, k - 1); // 5,6,7,4,3,2,1
+    reverse(k, nums.length - 1); //5,6,7,1,2,3,4
 };
