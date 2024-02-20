4015c4112c9e206e198339b054342f7bd3dc15ee
yellareddy-m <m.yellareddy6@gmail.com>
2024-02-20 21:22:05 +0530

    Time: 67 ms (43.68%), Space: 52 MB (37.78%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let currNode = head;
+    while (currNode && currNode.next) {
+        if ( currNode.val === currNode.next.val) {
+            currNode.next = currNode.next.next
+        } else  
+        currNode = currNode.next;
+    }
+    return head;
+};
